196107f495cd8fa8177e133b22e1801ddde83619
abdotha <abdotha5@gmail.com>
2024-04-12 17:51:40 +0200

    new

--- linkedList.cpp
+++ linkedList.cpp
@@ -5,10 +5,11 @@ int main()
 {
     LinkedList Test_list;
     Test_list.insertAtLast(8);
-    Test_list.insertAtFirst(5);
+    Test_list.insertAtFirst(7);
     Test_list.insertAt(2, 7);
     Test_list.insertAt(3, 10);
     Test_list.deleteAt(2);
+    Test_list.insertAt(1,6);
     Test_list.Show();
     
 }
